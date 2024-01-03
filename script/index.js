ac24d1a5dcab64c8cac6502b13fdf5407a5c15ae
eunji <dmswl7954@gmail.com>
2024-01-03 09:39:58 +0900

    프로젝트 체중계

---
+++ script/index.js
@@ -0,0 +1,19 @@
+// 적정 체중 구하는 계산기 만들기
+// 계산식) **적정체중 = (본인신장-100)*0.9**
+// 변수명 예) **userHeight, userWeight, goals, result, normal_w**
+// +CSS 디자인은 심미적으로 자유롭게 표현하세요.
+// - 현재 키와 몸무게를 자유롭게 입력하고
+// - 버튼을 눌렀을 때 최종 목표 칸에 메세지 출력하기
+// 최종목표 **결과예시) 적정체중은 ?kg이며 ?kg 초과되셨습니다.**
+const height = document.querySelector('#height'); 
+const weight = document.querySelector('#weight'); 
+const goals = document.querySelector('#goals');
+const result = document.querySelector('#result');
+console.log(height, weight, goals, result);
+result.addEventListener('click',function(){
+    height.value = 160;
+    weight.value = 50;
+    goals.value = Number(height.value-100)*0.9;
+    window.alert(`적정체중은 ${goals.value}kg이며 ${weight.value - goals.value}kg 초과되셨습니다.`);
+    bank_input.value = '';
+})
